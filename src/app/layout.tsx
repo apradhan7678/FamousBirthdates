749ab57552a532296eb63abef22697d0a10978eb
Abhushan Pradhan <apradhan7678@gmail.com>
2025-07-11 22:19:58 -0400

    update styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { DM_Sans, Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({
+const dmSans = DM_Sans({
   subsets: ["latin"],
   variable: "--font-sans",
+});
+
+const spaceMono = Space_Mono({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-mono",
 });
 
 export const metadata: Metadata = {
@@ -24,7 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.variable}>{children}</body>
+      <body className={`${dmSans.variable} ${spaceMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
